01a60d2343f1932c91016dd5db93d1d3919bb51c
Khushbu Lad <khushbunlad@gmail.com>
2023-05-05 15:03:11 +0530

    Allow repeat for service item

--- CarService/Controllers/ServiceItemMastersController.cs
+++ CarService/Controllers/ServiceItemMastersController.cs
@@ -67,19 +67,19 @@ namespace CarService.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("FldServiceItemId,FldItemName,FldItemType,FldQuanitityUnit,FldIdealAmount,FldHsnNumber")] TblServiceItemMaster tblServiceItemMaster)
         {
-            if (_context.TblServiceItemMasters.Count(s => s.FldHsnNumber == tblServiceItemMaster.FldHsnNumber) > 0)
-            {
-                ModelState.AddModelError("FldHsnNumber", "HSN/SAC number already used");
-            }
-            else
-            {
+            //if (_context.TblServiceItemMasters.Count(s => s.FldHsnNumber == tblServiceItemMaster.FldHsnNumber) > 0)
+            //{
+            //    ModelState.AddModelError("FldHsnNumber", "HSN/SAC number already used");
+            //}
+            //else
+            //{
                 if (ModelState.IsValid)
                 {
                     _context.Add(tblServiceItemMaster);
                     await _context.SaveChangesAsync();
                     return RedirectToAction(nameof(Index));
                 }
-            }
+           // }
             return View(tblServiceItemMaster);
         }
 
@@ -110,10 +110,10 @@ namespace CarService.Controllers
             {
                 return NotFound();
             }
-            if (_context.TblServiceItemMasters.Count(s => s.FldHsnNumber == tblServiceItemMaster.FldHsnNumber && s.FldServiceItemId != tblServiceItemMaster.FldServiceItemId) > 0)
-            {
-                ModelState.AddModelError("FldHsnNumber", "HSN/SAC number already used");
-            }
+            //if (_context.TblServiceItemMasters.Count(s => s.FldHsnNumber == tblServiceItemMaster.FldHsnNumber && s.FldServiceItemId != tblServiceItemMaster.FldServiceItemId) > 0)
+            //{
+            //    ModelState.AddModelError("FldHsnNumber", "HSN/SAC number already used");
+            //}
             if (ModelState.IsValid)
             {
                 try
